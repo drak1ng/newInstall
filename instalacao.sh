5777e4a68fc063c8013c0a3de1d37e7903532e32
drak1ng <rodrigo.misquini@gmail.com>
2021-02-24 20:09:01 -0300

    Update instalacao.sh

--- instalacao.sh
+++ instalacao.sh
@@ -1,47 +1,101 @@
-printf "\n"
-echo -e "\e[1;32mINICIANDO INSTALAÇÃO\e[0m" 
+#!/bin/bash
 
-echo "Iniciando Script"
+printf "\n"
+echo "INICIANDO INSTALAÇÃO" 
+printf "\n"
 
 # Check for Homebrew, install if we don't have it
 if test ! $(which brew); then
-    echo "Instalando Homebrew..."
+    echo -e "\033[1;34mInstalando Homebrew... \033[0m"
+    printf "\n"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 # Update homebrew recipes
+echo -e "\033[1;34mAtualizando Homebrew... \033[0m"
 brew update
 brew upgrade
 
-printf "\n\nInstalando Aplicativos...\n"
-brew install --cask appcleaner
-brew install --cask balenaetcher
-brew install --cask coda
-brew install --cask firefox
-brew install --cask google-chrome
-brew install --cask hyper
-brew install --cask iina
-brew install --cask imageoptim
-brew install --cask insomnia
-brew install --cask visual-studio-code
-brew install --cask opencore-configurator
-brew install --cask openemu
-brew install --cask plex-media-server
-brew install --cask sequel-ace
-brew install --cask spotify
-brew install --cask the-unarchiver
-brew install --cask transmission
-brew install --cask vlc
-brew install --cask whatsapp
+echo -e "\033[1;31m"
+read -p "Deseja instalar Fonts? [Y/n] " response
+echo -e "\033[0m"
+case "$response" in [yY][eE][sS]|[yY]) 
+        brew install --cask font-fira-code
+        brew install --cask font-hack-nerd-font
+    ;;
+esac
 
-printf "\n\nInstalando Lojas...\n"
-brew install --cask steam
-brew install --cask battle-net
-brew install --cask epic-games
+echo -e "\033[1;31m"
+read -p "Deseja instalar Navegadores? [Y/n] " response
+echo -e "\033[0m"
+case "$response" in [yY][eE][sS]|[yY]) 
+        brew install --cask google-chrome
+        brew install --cask firefox
+        brew install --cask microsoft-edge
+    ;;
+esac
 
-printf "\n\nInstalando Cordova...\n"
-brew install node
-npm install -g cordova
+echo -e "\033[1;31m"
+read -p "Deseja instalar Utilitários? [Y/n] " response
+echo -e "\033[0m"
+case "$response" in [yY][eE][sS]|[yY]) 
+        brew install --cask appcleaner
+        brew install --cask spotify
+        brew install --cask the-unarchiver
+        brew install --cask transmission
+        brew install --cask vlc
+        brew install --cask iina
+        brew install --cask whatsapp
+        brew install --cask plex-media-server
+    ;;
+esac
+
+echo -e "\033[1;31m"
+read -p "Deseja instalar Loja de Jogos? [Y/n] " response
+echo -e "\033[0m"
+case "$response" in [yY][eE][sS]|[yY]) 
+        brew install --cask steam
+        brew install --cask battle-net
+        brew install --cask epic-games
+        brew install --cask openemu
+    ;;
+esac
+
+echo -e "\033[1;31m"
+read -p "Deseja instalar Ferramentas de Programação? [Y/n] " response
+echo -e "\033[0m"
+case "$response" in [yY][eE][sS]|[yY]) 
+        brew install --cask coda
+        brew install --cask sequel-ace
+        brew install --cask imageoptim
+    ;;
+esac
+
+echo -e "\033[1;31m"
+read -p "Deseja instalar Ferramentas de Adicionais? [Y/n] " response
+echo -e "\033[0m"
+case "$response" in [yY][eE][sS]|[yY]) 
+        brew install --cask hyper
+        brew install --cask insomnia
+        brew install --cask visual-studio-code
+        brew install --cask balenaetcher
+        brew install --cask ngrok
+        brew install --cask opencore-configurator
+    ;;
+esac
+
+echo -e "\033[1;31m"
+read -p "Deseja instalar Cordova? [Y/n] " response
+echo -e "\033[0m"
+case "$response" in [yY][eE][sS]|[yY]) 
+        brew install node
+        npm install -g cordova
+        brew install openjdk@8
+        brew install --cask adoptopenjdk8
+    ;;
+esac
+    
 
 printf "\n\n"
-echo -e "\e[1;32mSua instalção foi concluída!\e[0m" 
+echo -e "\033[1;32mINSTALAÇÃO CONCLUÍDA! \033[0m"
+printf "\n"
