43075d79bff1b5abd2915a64b2c9b5913bd5c613
drak1ng <rodrigo.misquini@gmail.com>
2021-02-24 18:06:30 -0300

    Update instalacao.sh

--- instalacao.sh
+++ instalacao.sh
@@ -11,31 +11,31 @@ brew update
 brew upgrade
 
 printf "\n\nInstalando Aplicativos..."
-#brew install --cask appcleaner
-#brew install --cask balenaetcher
-#brew install --cask coda
-#brew install --cask firefox
-#brew install --cask google-chrome
-#brew install --cask hyper
-#brew install --cask iina
-#brew install --cask imageoptim
-#brew install --cask insomnia
-#brew install --cask visual-studio-code
-#brew install --cask opencore-configurator
-#brew install --cask openemu
-#brew install --cask plex-media-server
-#brew install --cask sequel-ace
-#brew install --cask spotify
-#brew install --cask the-unarchiver
-#brew install --cask transmission
-#brew install --cask vlc
-#brew install --cask whatsapp
+brew install --cask appcleaner
+brew install --cask balenaetcher
+brew install --cask coda
+brew install --cask firefox
+brew install --cask google-chrome
+brew install --cask hyper
+brew install --cask iina
+brew install --cask imageoptim
+brew install --cask insomnia
+brew install --cask visual-studio-code
+brew install --cask opencore-configurator
+brew install --cask openemu
+brew install --cask plex-media-server
+brew install --cask sequel-ace
+brew install --cask spotify
+brew install --cask the-unarchiver
+brew install --cask transmission
+brew install --cask vlc
+brew install --cask whatsapp
 
 printf "\n\nInstalando Lojas..."
-#brew install --cask steam
-#brew install --cask battle-net
-#brew install --cask epic-games
+brew install --cask steam
+brew install --cask battle-net
+brew install --cask epic-games
 
 printf "\n\nInstalando Cordova..."
-#brew install node
-#npm install -g cordova
+brew install node
+npm install -g cordova
