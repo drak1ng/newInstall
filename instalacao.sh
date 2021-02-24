d1ede926e2e2caa1eb240cec52e40007c057a22e
drak1ng <rodrigo.misquini@gmail.com>
2021-02-24 18:05:00 -0300

    Update instalacao.sh

--- instalacao.sh
+++ instalacao.sh
@@ -2,7 +2,7 @@ echo "Iniciando Script"
 
 # Check for Homebrew, install if we don't have it
 if test ! $(which brew); then
-    echo "Instalando homebrew..."
+    echo "Iniciando Instalação..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
@@ -10,32 +10,32 @@ fi
 brew update
 brew upgrade
 
-echo "\n\nInstalando Aplicativos..."
-brew install --cask appcleaner
-brew install --cask balenaetcher
-brew install --cask coda
-brew install --cask firefox
-brew install --cask google-chrome
-brew install --cask hyper
-brew install --cask iina
-brew install --cask imageoptim
-brew install --cask insomnia
-brew install --cask visual-studio-code
-brew install --cask opencore-configurator
-brew install --cask openemu
-brew install --cask plex-media-server
-brew install --cask sequel-ace
-brew install --cask spotify
-brew install --cask the-unarchiver
-brew install --cask transmission
-brew install --cask vlc
-brew install --cask whatsapp
+printf "\n\nInstalando Aplicativos..."
+#brew install --cask appcleaner
+#brew install --cask balenaetcher
+#brew install --cask coda
+#brew install --cask firefox
+#brew install --cask google-chrome
+#brew install --cask hyper
+#brew install --cask iina
+#brew install --cask imageoptim
+#brew install --cask insomnia
+#brew install --cask visual-studio-code
+#brew install --cask opencore-configurator
+#brew install --cask openemu
+#brew install --cask plex-media-server
+#brew install --cask sequel-ace
+#brew install --cask spotify
+#brew install --cask the-unarchiver
+#brew install --cask transmission
+#brew install --cask vlc
+#brew install --cask whatsapp
 
-echo "\n\nInstalando Lojas..."
-brew install --cask steam
-brew install --cask battle-net
-brew install --cask epic-games
+printf "\n\nInstalando Lojas..."
+#brew install --cask steam
+#brew install --cask battle-net
+#brew install --cask epic-games
 
-echo "\n\nInstalando Cordova..."
-brew install node
-npm install -g cordova
+printf "\n\nInstalando Cordova..."
+#brew install node
+#npm install -g cordova
